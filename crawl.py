7aa29e250c5b65c6f49f7a1b5bd6db5a17653b02
Tientryhard <tienvm886@gmail.com>
2026-01-03 22:59:21 +0700

    config a base url

---
+++ crawl.py
@@ -0,0 +1,17 @@
+import asyncio
+import aiohttp
+from typing import Optional, Dict, List, Tuple
+
+from tqdm import tqdm  # <-- use normal tqdm for loops
+# ================= CONFIG =================
+
+BASE_DOMAIN = "https://vietnamnet.vn/newsapi-edu/EducationStudentScore/CheckCandidateNumber"
+COMPONENT_ID = "ComponentId=COMPONENT002298"
+PAGE_ID = "PageId=fa4119c27edb45558886cde08459bb1b"
+YEAR = "2024"
+TYPE = "type=2"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "application/json",
+}
